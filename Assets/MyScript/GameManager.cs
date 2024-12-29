a9a242e62a1e15dc38537e7fdd583ec8a91121ee
DESKTOP-020BEL1\PC_User <ko.he.t.4.16@gmail.com>
2024-12-30 00:08:13 +0900

    投稿

--- Assets/MyScript/GameManager.cs
+++ Assets/MyScript/GameManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 using UnityEngine.UI;
-using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
